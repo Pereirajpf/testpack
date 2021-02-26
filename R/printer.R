d358343b0e72351b0952d310a25ba1ea3e4fcbb2
Pereirajpf <pereira.jpf96@gmail.com>
2021-02-26 12:01:56 +0000

    updated the vignettes and added documentation for the printer function

--- R/printer.R
+++ R/printer.R
@@ -1,3 +1,19 @@
-printer <- function(r, x, y){
-  print(paste0(" x = ",x))
+#' Print funct
+#'
+#' @param r second column
+#' @param x first column
+#'
+#' @return A tibble
+#' @export
+#'
+#'@importFrom tibble tibble
+#'@importFrom utils head adist
+#'
+#' @examples
+#' printer(x = rnorm(5), r = rnorm(5))
+printer <- function(r, x){
+  x = tibble(x = x, r = r)
+  print(head(x))
+  return(x)
 }
+
